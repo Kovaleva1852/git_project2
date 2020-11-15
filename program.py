2d2e4120dd7a90442195813264285bbb169cd719
Kovaleva1852 <74491795+Kovaleva1852@users.noreply.github.com>
2020-11-15 14:16:22 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 def main():
     print('My First git program')
     print('Add I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    print('Hello')
 
 
 if __name__ == '__main__':
